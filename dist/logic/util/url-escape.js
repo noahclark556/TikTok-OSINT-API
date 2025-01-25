e47aa09741671ba3ede4a27a6fa1ef7374292082
Noah <noahclark556@gmail.com>
2025-01-24 19:41:43 -0500

    Update url-escape.js

--- dist/logic/util/url-escape.js
+++ dist/logic/util/url-escape.js
@@ -4,6 +4,6 @@ function escapeQueryJson(url) {
     let enc = encodeURIComponent(spl[3]);
     return `${spl[0]}=${spl[1]}=${spl[2]}=${enc}`;
 }
-const originalUrl = 'http://localhost:8080/osintx?username=realdonaldtrump&apikey=noahclark556&query={"bio":true,"name":true,"following":true,"followers":true,"posts":5,"aidescription":true}';
+const originalUrl = 'http://localhost:8080/osintx?username=realdonaldtrump&apikey=customapikey&query={"bio":true,"name":true,"following":true,"followers":true,"posts":5,"aidescription":true}';
 const escapedUrl = escapeQueryJson(originalUrl);
 console.log(`curl -X GET "${escapedUrl}"`);
